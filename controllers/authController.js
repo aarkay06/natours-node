964320326b61e73613431fc74e536bc65c04cccb
KrishFX <88158135+aarkay06@users.noreply.github.com>
2026-02-14 07:17:09 +0000

    Feb 14, 2026, 12:47 PM

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,11 +1,15 @@
 const catchAsync = require('../utils/catchAsync');
 const User = require('./../models/userModel');
+const jsw = require('jsonwebtoken');
 
 exports.signup = catchAsync(async (req, res, next) => {
   const newUser = await User.create(req.body);
-
+  const token = jsw.sign({ id: newUser._id }, process.env.JSW_SECRET_KEY, {
+    expiresIn: process.env.JSW_EXPIRES_IN,
+  });
   res.status(201).json({
     status: 'success',
+    token,
     data: {
       user: newUser,
     },
--- controllers/tourControllers.js
+++ controllers/tourControllers.js
@@ -2,6 +2,7 @@ const AppError = require('../utils/appError');
 const Tour = require('./../models/tourModel');
 const APIFeatures = require('./../utils/apiFeatures');
 const catchAsync = require('./../utils/catchAsync');
+const AppError = require('../utils/appError');
 
 exports.aliasTopTours = (req, res, next) => {
   req.query.limit = 5;
