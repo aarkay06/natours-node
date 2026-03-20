1a7952ab2e76ea0212ef169ab9d843ba9956a9e1
KrishFX <88158135+aarkay06@users.noreply.github.com>
2026-03-20 16:02:13 +0000

    Mar 20, 2026, 9:32 PM

--- controllers/authController.js
+++ controllers/authController.js
@@ -5,7 +5,6 @@ const jsw = require('jsonwebtoken');
 exports.signup = catchAsync(async (req, res, next) => {
   const newUser = await User.create({
     name: req.body.name,
-
     email: req.body.email,
     password: req.body.password,
     passwordConfirm: req.body.passwordConfirm,
