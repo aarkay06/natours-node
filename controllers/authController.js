91848bd7e017f5d1e9f7c1aaa955bb9e7a2d6143
KrishFX <88158135+aarkay06@users.noreply.github.com>
2026-03-28 17:36:09 +0000

    Mar 28, 2026, 5:36 AM

--- controllers/authController.js
+++ controllers/authController.js
@@ -27,17 +27,20 @@ exports.signup = catchAsync(async (req, res, next) => {
 exports.login = catchAsync(async (req, res, next) => {
   const { email, password } = req.body;
 
-  // 1. Check if email and password exits
+  // 1) Check if email and password exist
   if (!email || !password) {
-    return next(new AppError('Please provide email and password.', 400));
+    return next(new AppError('Please provide email and password!', 400));
   }
 
-  // 2. Check if the user exists and the password is correct
+  // 2) Check if user exists && password is correct
+  // We MUST explicitly select('+password') because of select: false in schema
   const user = await User.findOne({ email }).select('+password');
-  console.log(user);
-  const token = '';
-  res.status(200).json({
-    status: 'success',
-    token,
-  });
+
+  if (!user || !(await user.correctPassword(password, user.password))) {
+    return next(new AppError('Incorrect email or password', 401));
+  }
+
+  // 3) If everything ok, send token to client
+  const token = signToken(user._id);
+  res.status(200).json({ status: 'success', token });
 });
