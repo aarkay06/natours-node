2e157aefce387357a9a14522244f7a4975be6a2c
KrishFX <88158135+aarkay06@users.noreply.github.com>
2026-03-28 20:30:46 +0000

    Mar 28, 2026, 8:30 AM

--- models/userModel.js
+++ models/userModel.js
@@ -52,5 +52,6 @@ userSchema.methods.correctPassword = async function (
 ) {
   return await bcrypt.compare(candidatePassword, userPassword);
 };
+
 const User = mongoose.model('User', userSchema);
 module.exports = User;
