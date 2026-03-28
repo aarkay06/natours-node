a9549581122598772fb0da4df61d8e98dfa353d6
KrishFX <88158135+aarkay06@users.noreply.github.com>
2026-03-28 21:40:50 +0000

    Mar 28, 2026, 9:40 AM

--- controllers/authController.js
+++ controllers/authController.js
@@ -2,6 +2,7 @@ const catchAsync = require('../utils/catchAsync');
 const User = require('./../models/userModel');
 const jwt = require('jsonwebtoken');
 const AppError = require('../utils/appError');
+const { promisify } = require('util');
 
 const signToken = (id) => {
   return jwt.sign({ id }, process.env.JSW_SECRET_KEY, {
