954c4dde03c32571522fd2e23d7d7ee59450991c
KrishFX <88158135+aarkay06@users.noreply.github.com>
2026-03-28 16:21:56 +0000

    Mar 28, 2026, 4:21 AM

--- models/userModel.js
+++ models/userModel.js
@@ -46,5 +46,11 @@ userSchema.pre('save', async function (next) {
   next();
 });
 
+userSchema.method.correctPassword = async function (
+  candidatePassword,
+  userPassword,
+) {
+  return await bcrypt.compare(candidatePassword, userPassword);
+};
 const User = mongoose.model('User', userSchema);
 module.exports = User;
